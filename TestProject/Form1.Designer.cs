43ee8eeee312b8fda96f8037d588340bd05b72e8
ZdenekPetrik <zdenekpetrik@seznam.cz>
2022-06-06 11:13:18 +0200

    Jen drobnost - přejmenování buttonu na LINQ

--- TestProject/Form1.Designer.cs
+++ TestProject/Form1.Designer.cs
@@ -28,25 +28,25 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.button1 = new System.Windows.Forms.Button();
+      this.btnLinq = new System.Windows.Forms.Button();
       this.SuspendLayout();
       // 
-      // button1
+      // btnLinq
       // 
-      this.button1.Location = new System.Drawing.Point(12, 36);
-      this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(116, 33);
-      this.button1.TabIndex = 0;
-      this.button1.Text = "button1";
-      this.button1.UseVisualStyleBackColor = true;
-      this.button1.Click += new System.EventHandler(this.button1_Click);
+      this.btnLinq.Location = new System.Drawing.Point(12, 36);
+      this.btnLinq.Name = "btnLinq";
+      this.btnLinq.Size = new System.Drawing.Size(116, 33);
+      this.btnLinq.TabIndex = 0;
+      this.btnLinq.Text = "LINQ";
+      this.btnLinq.UseVisualStyleBackColor = true;
+      this.btnLinq.Click += new System.EventHandler(this.button1_Click);
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(284, 261);
-      this.Controls.Add(this.button1);
+      this.Controls.Add(this.btnLinq);
       this.Name = "Form1";
       this.Text = "Form1";
       this.ResumeLayout(false);
@@ -55,7 +55,7 @@
 
     #endregion
 
-    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button btnLinq;
   }
 }
 
